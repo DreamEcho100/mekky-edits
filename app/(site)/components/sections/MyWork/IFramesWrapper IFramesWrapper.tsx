c28ff4193acf53d3c4132b4b008359cf49b935aa
DreamEcho100 <maze6572198@gmail.com>
2023-05-12 16:55:38 +0300

    Minor update

--- app/(site)/components/sections/MyWork/IFramesWrapper IFramesWrapper.tsx
+++ app/(site)/components/sections/MyWork/IFramesWrapper IFramesWrapper.tsx
@@ -2,7 +2,7 @@
 
 import { cx } from 'class-variance-authority';
 import dynamic from 'next/dynamic';
-import { useRef } from 'react';
+import { Suspense, useRef } from 'react';
 import { useIntersectionObserver } from '~/app/utils/hooks';
 
 const Iframe = dynamic(() => import('~/app/components/common/Iframe'), {
@@ -29,32 +29,36 @@ const IFramesWrapper = () => {
 
 	return (
 		<div className='flex gap-8'>
-			<Iframe
-				ref={iframeContainerRef1}
-				containerProps={{
-					className: cx(
-						'perspective-animation rotate-[25deg] mx-auto max-w-full aspect-ratio-1-1 sm:aspect-ratio-16-9',
-						'transition-all duration-500 delay-500'
-					)
-				}}
-				title='vimeo-player'
-				src='https://player.vimeo.com/video/822644379?h=4b7aafc862'
-				width={640}
-				height={300}
-			/>
-			<Iframe
-				ref={iframeContainerRef2}
-				containerProps={{
-					className: cx(
-						'perspective-animation rotate-[25deg] mx-auto max-w-full aspect-ratio-1-1 sm:aspect-ratio-16-9',
-						'transition-all duration-500 delay-100'
-					)
-				}}
-				src='https://player.vimeo.com/video/822643020?h=f837ecca64'
-				width={640}
-				height={680}
-				allow='autoplay; fullscreen; picture-in-picture'
-			/>
+			<Suspense fallback={<div style={{ width: 640, height: 300 }} />}>
+				<Iframe
+					ref={iframeContainerRef1}
+					containerProps={{
+						className: cx(
+							'perspective-animation rotate-[25deg] mx-auto max-w-full aspect-ratio-1-1 sm:aspect-ratio-16-9',
+							'transition-all duration-500 delay-500'
+						)
+					}}
+					title='vimeo-player'
+					src='https://player.vimeo.com/video/822644379?h=4b7aafc862'
+					width={640}
+					height={300}
+				/>
+			</Suspense>
+			<Suspense fallback={<div style={{ width: 640, height: 680 }} />}>
+				<Iframe
+					ref={iframeContainerRef2}
+					containerProps={{
+						className: cx(
+							'perspective-animation rotate-[25deg] mx-auto max-w-full aspect-ratio-1-1 sm:aspect-ratio-16-9',
+							'transition-all duration-500 delay-100'
+						)
+					}}
+					src='https://player.vimeo.com/video/822643020?h=f837ecca64'
+					width={640}
+					height={680}
+					allow='autoplay; fullscreen; picture-in-picture'
+				/>
+			</Suspense>
 		</div>
 	);
 };
